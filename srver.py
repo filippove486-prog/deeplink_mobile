c4a10d645084db8eaed9ac1ccae22394b9ffce9e
filippove486-prog <filippove486@gmail.com>
2026-01-18 21:44:56 +0300

    Update srver.py

--- srver.py
+++ srver.py
@@ -353,5 +353,11 @@ def handle_typing(data):
         'is_typing': is_typing
     }, room=f'chat_{chat_id}')
 
-if __name__ == '__main__':
-    socketio.run(app, debug=True, host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    socketio.run(
+        app,
+        host="0.0.0.0",
+        port=5000,
+        allow_unsafe_werkzeug=True
+    )
+
